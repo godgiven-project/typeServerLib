d078c992afaeae82773abfa63cb22ae5df411149
asmanp2012 <asmanp2012@gmail.com>
2022-02-23 11:46:35 +0330

    feat(demo): Add util page

--- demo/src/page/index.ts
+++ demo/src/page/index.ts
@@ -1,2 +1,3 @@
 export * from './home.js';
 export * from './test.js';
+export * from './util.js';
--- demo/src/server.ts
+++ demo/src/server.ts
@@ -1,6 +1,6 @@
 import { App } from '@godgiven/type-server';
 import { authFunction } from './middleware/authentication-server.js';
-import { pageHome, pageTest } from './page/index.js';
+import { pageHome, pageTest, pageUtil } from './page/index.js';
 
 const app = new App();
 app.port = 5000;
@@ -8,6 +8,7 @@ app.version = 'v1';
 app.middlewareList.push(authFunction);
 
 app.register('GET', '/', pageHome);
+app.register('GET', '/util', pageUtil);
 app.register('GET', '/test', pageTest);
 app.register('GET', '', pageHome);
 
