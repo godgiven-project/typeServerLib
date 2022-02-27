29b88e6eeab30af452d5604d8a41b4960e8c4c66
asmanp2012 <asmanp2012@gmail.com>
2022-02-27 12:01:20 +0330

    feat(typeServer/responseInterface) Add Array to data type

--- package/type-server/src/type.ts
+++ package/type-server/src/type.ts
@@ -2,10 +2,10 @@ export type methodType = 'GET' | 'POST';
 
 export interface ApiResponseInterface
 {
-  [key: string]: string | number | boolean | Record<string, unknown> | undefined;
+  [key: string]: string | number | boolean | Record<string, unknown> | undefined | Array<Record<string, unknown>>;
   ok: boolean;
   errorCode?: number;
   description: string;
-  data?: Record<string, unknown>;
+  data?: Record<string, unknown> | Array<Record<string, unknown>>;
   apiVersion?: string;
 }
