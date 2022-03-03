1a4442ee67e8728e4613275da7a222b86444a788
asmanp2012 <asmanp2012@gmail.com>
2022-03-03 10:59:37 +0330

    feat(json-file): writeJsonFile overwrite

--- package/json-file/src/write-json-file.ts
+++ package/json-file/src/write-json-file.ts
@@ -1,27 +1,39 @@
 import { existsSync, promises as fs } from 'fs';
 import { resolve, dirname } from 'path';
+
+// eslint-disable-next-line @typescript-eslint/prefer-as-const
+const trueValue: true = true;
+
 /**
  *
  */
-export async function writeJsonFile(path: string, data: unknown): Promise<void>
+export async function writeJsonFile(path: string, data: unknown, overwrite: boolean = true): Promise<Error | true>
 {
-  // Check the path is exsist
-  path = resolve(path);
-  if (!existsSync(path))
-  {
-    await fs.mkdir(dirname(path), { recursive: true });
-  }
+  // 'w' - Open file for writing. The file is created (if it does not exist) or truncated (if it exists).
+  // 'wx' - Like 'w' but fails if path exists.
+  const config = { flag: 'w' };
 
+  // Check the path is exist
+  path = resolve(path);
+  if (!existsSync(path)) { await fs.mkdir(dirname(path), { recursive: true }); }
+
+  if (overwrite === false) { config.flag = 'wx'; }
   // Convert json to string
   const json = JSON.stringify(data, undefined, 2);
 
   // Write string to file
-  try
+  return fs.writeFile(
+    path,
+    json,
+    {
+      ...config,
+      encoding: 'utf-8'
+    }
+  ).then(() =>
   {
-    await fs.writeFile(path, json, { encoding: 'utf-8' });
-  }
-  catch (err)
+    return trueValue;
+  }).catch((err) =>
   {
-    throw new Error('write_file_error');
-  }
+    return new Error(err.code);
+  });
 }
