ce20e796408ad29f2830764a8b6bca845be3774c
Seyed Morteza Haghiralsadat <smhboc@gmail.com>
2022-12-04 23:03:39 +0330

    feat(Database/deleteById)

---
+++ demo/src/page/database-delete-by-id.ts
@@ -0,0 +1,38 @@
+import { ServerResponse } from 'http';
+import { requestType } from '../middleware/authentication-server';
+import { sendResponse } from '@godgiven/type-server';
+import { Database } from '@godgiven/database/json-file.js';
+import Debug from 'debug';
+
+const log = Debug('app/page/database/deleteById');
+
+const Db = new Database({
+  name: 'testDb',
+  path: './data',
+});
+
+export const pageDeleteUniqueIdDatabase = async (_request: requestType, response: ServerResponse): Promise<void> =>
+{
+  log('pageUpdateDatabase');
+  try
+  {
+    await Db.deleteById(
+      'testTable',
+      'test'
+    );
+    sendResponse(response, 200, {
+      ok: true,
+      description: '..:: Welcome ::..',
+    });
+  }
+  catch (error)
+  {
+    sendResponse(response, 200, {
+      ok: false,
+      description: '..:: Welcome ::..',
+      data: {
+        message: (error as Error).message
+      },
+    });
+  }
+};
